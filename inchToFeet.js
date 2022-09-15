84c706ea9d8db06d2fd0856896ebc3037827599e
Rabiul Islam <52354711+rabi993@users.noreply.github.com>
2022-09-16 05:08:54 +0600

    function apply

---
+++ inchToFeet.js
@@ -0,0 +1,24 @@
+// var inch=156;
+// var feet =inch/12;
+// console.log(feet);
+// console.log(feet);
+// console.log(feet);
+
+function inchToFeet (inch){
+    var feet = inch/12;
+    return feet;
+}
+// var nanafeet =inchToFeet(24);
+// var rahatfeet =inchToFeet(54);
+// var dadafeet =inchToFeet(84);
+// console.log(nanafeet);
+// console.log(rahatfeet);
+// console.log(dadafeet);
+
+var part=[24,54,84];
+var nanafeet =inchToFeet(part[0]);
+var rahatfeet =inchToFeet(part[1]);
+var dadafeet =inchToFeet(part[2]);
+console.log(nanafeet);
+console.log(rahatfeet);
+console.log(dadafeet);
