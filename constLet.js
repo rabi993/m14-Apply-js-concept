45737cbee2eb027eb08b9d1f27b03cf120ea84e3
Rabiul Islam <52354711+rabi993@users.noreply.github.com>
2022-09-16 05:31:04 +0600

    variable declear . LET & CONS

---
+++ constLet.js
@@ -0,0 +1,24 @@
+var name ='javed akbar';
+// console.log(name.length);
+if(name.length>4){
+    name= 'juvu';
+}
+console.log(name);
+
+
+var name ='jav';
+if(name.length>4){
+    name= 'juvu';
+}
+console.log(name);
+
+// rydtfyigop[liuytrewrtyuidsfhgl;kljhdsa
+// var age =14; 
+let age =14; //re assign er jonno let use
+age= 34;
+age=65;
+console.log(age);
+// var country ='Bangladesh';
+const country ='Bangladesh';
+console.log(country);
+
