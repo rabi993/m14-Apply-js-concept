ca7e68fd92f451bbf4f1444ad0aeaea9b0d63607
Rabiul Islam <52354711+rabi993@users.noreply.github.com>
2022-09-16 06:45:36 +0600

    factorial on function

--- factorial.js
+++ factorial.js
@@ -1,15 +1,28 @@
-num=5;
-let result=1;
-for(let i=num; i>=1; i--){
-    
-     result= result*i;
-}
-const factorial= result;
-console.log(num, factorial)
+// num=5;
+// let result=1;
+// for(let i=num; i>=1; i--){
+//      result= result*i;
+// }
+// const factorial= result;
+// console.log(num, factorial)
+
 
 // var factorial=1
 // for(let i=1; i<=5; i++){
-   
 //     factorial =factorial*i;
 //     console.log(i, factorial)
 // }
+
+
+function factorial(num){
+     let result=1;
+     for(let i=num; i>=1; i--){
+     result= result*i;
+}
+ return result;
+}
+number=4;
+const checkFact= factorial(number);
+num=
+console.log('num! = result');
+console.log( number,'! =',checkFact);
