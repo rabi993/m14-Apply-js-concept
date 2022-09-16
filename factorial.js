4ce4b53632f09aedbd5c5b288ddea08e6cc1a5cb
Rabiul Islam <52354711+rabi993@users.noreply.github.com>
2022-09-16 06:29:01 +0600

    factorial on for

---
+++ factorial.js
@@ -0,0 +1,15 @@
+num=5;
+let result=1;
+for(let i=num; i>=1; i--){
+    
+     result= result*i;
+}
+const factorial= result;
+console.log(num, factorial)
+
+// var factorial=1
+// for(let i=1; i<=5; i++){
+   
+//     factorial =factorial*i;
+//     console.log(i, factorial)
+// }
